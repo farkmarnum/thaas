4e567bdc2354fa7dbdd4ac721f23f8a4405c9bc6
farkmarnum <mark@nayya.com>
2022-07-30 01:03:33 -0400

    Switch from just function to API

--- packages/backend/handler.js
+++ packages/backend/handler.js
@@ -1,15 +1,23 @@
-'use strict';
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'booya!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-};
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+module.exports.handler = serverless(app);

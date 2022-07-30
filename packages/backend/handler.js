6514902affc93d0fd2a3d215aeff37e8fce6114b
farkmarnum <mark@nayya.com>
2022-07-30 00:54:38 -0400

    test

--- packages/backend/handler.js
+++ packages/backend/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
+        message: 'booya!',
         input: event,
       },
       null,

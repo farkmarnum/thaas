9d674712177b0fa0f6964905a38b81a7ea77446f
farkmarnum <mark@nayya.com>
2022-07-30 02:51:41 -0400

    Fix

--- packages/web/backend/routes/api.js
+++ packages/web/backend/routes/api.js
@@ -1,25 +1,24 @@
 const router = require('express').Router();
 
-
-app.get("tom", (req, res, next) => {
+router.get("tom", (req, res, next) => {
   return res.status(200).json({
     message: "TODO: return an image.",
   });
 });
 
-app.get("slack", (req, res, next) => {
+router.get("slack", (req, res, next) => {
   return res.status(200).json({
     message: "TODO: API for Slack integration.",
   });
 });
 
-app.get("slack/oauth", (req, res, next) => {
+router.get("slack/oauth", (req, res, next) => {
   return res.status(200).json({
     message: "TODO: handle OAuth for Slack integration (?).",
   });
 });
 
-app.get("github", (req, res, next) => {
+router.get("github", (req, res, next) => {
   return res.status(200).json({
     message: "TODO: API for GitHub integration.",
   });

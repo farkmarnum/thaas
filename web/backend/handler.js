928d7a7eb985f5e8a0019037cebbb9ffa67eb88c
farkmarnum <mark@nayya.com>
2022-07-31 00:40:30 -0400

    cleanup

--- web/backend/handler.js
+++ web/backend/handler.js
@@ -1,23 +1,13 @@
-const serverless = require("serverless-http");
-const express = require("express");
-const app = express();
+const handler = (event) => {
+  console.log(event)
 
-app.get("/", (_req, res) => {
-  return res.status(400).json({
-    message: "Usage: GET /api/v1/tom",
-  });
-});
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: json.dumps({ received: event }),
+  }
+}
 
-app.get("/health", (_req, res) => {
-  return res.sendStatus(200);
-});
-
-app.use('/api/v1', require('./routes/api'));
-
-app.use((_req, res) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
-});
-
-module.exports.handler = serverless(app);
+module.exports = { handler };

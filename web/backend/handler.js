710d90eb84890cda7d07e415cdb887075a6b4996
farkmarnum <mark@nayya.com>
2022-07-31 11:15:08 -0400

    Add JSON.parse

--- web/backend/handler.js
+++ web/backend/handler.js
@@ -6,14 +6,12 @@ const api = require('./helpers/api');
 
 const main = async (event) => {
   const {
-    body: params,
+    body,
     rawPath: path,
     requestContext: {
       http: { method },
     },
   } = event;
-
-  console.info(event);
 
   if (path === '/health' && method === 'GET') {
     return formatTextResponse('OK');
@@ -22,6 +20,7 @@ const main = async (event) => {
   if (path.startsWith('/api/v1')) {
     const subPath = path.replace(/^\/api\/v1/, '');
 
+    const params = JSON.parse(body);
     const response = api(subPath, method, params);
 
     if (response) {

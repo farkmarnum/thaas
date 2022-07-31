2ad6d993262093c283845020c3971053d32d7330
farkmarnum <mark@nayya.com>
2022-07-30 22:34:03 -0400

    Fix submodule issue

---
+++ action/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+  },
+};
---
+++ action/src/callApi.ts
@@ -0,0 +1,5 @@
+const callApi = async (): Promise<void> => {};
+
+// https://api.thaas.io/api/v1/integrations/github
+
+export default callApi;

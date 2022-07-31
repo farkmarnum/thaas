5dde2d3692220cad229c675da95452399f60facd
farkmarnum <mark@nayya.com>
2022-07-31 01:13:36 -0400

    Fix

---
+++ web/backend/helpers/formatResponse.js
@@ -0,0 +1,11 @@
+const formatResponse = response => ({
+  statusCode: 200,
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(response),
+});
+
+module.exports = {
+  formatResponse,
+};

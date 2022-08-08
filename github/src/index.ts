709a34468b009770b93c9676c521310807e28c57
farkmarnum <mark@nayya.com>
2022-08-08 17:48:00 -0400

    WIP 1

--- github/src/index.ts
+++ github/src/index.ts
@@ -1,19 +1,38 @@
 import fetch from 'node-fetch';
 import { Probot, Context } from "probot";
+import {
+  PullRequestReviewSubmittedEvent,
+  IssueCommentCreatedEvent,
+  CommitCommentCreatedEvent,
+  PullRequestReviewCommentCreatedEvent,
+} from "@octokit/webhooks-types/schema";
 
 const hasCommand = (commentBody: string | null | undefined) => commentBody && /^!hanks\b/m.test(commentBody);
+
+const API_URL = 'https://api.thaas.io/api/v1/integrations/github';
+const HOMEPAGE = 'https://thaas.io';
 
 const ISSUE_COMMENT_CREATED = 'issue_comment.created';
 const COMMIT_COMMENT_CREATED = 'commit_comment.created';
 const PR_DIFF_COMMENT_CREATED = 'pull_request_review_comment.created';
 const PR_REVIEW_CREATED = 'pull_request_review.submitted';
 
-type CommentEvents = typeof ISSUE_COMMENT_CREATED | typeof COMMIT_COMMENT_CREATED | typeof PR_DIFF_COMMENT_CREATED
-type ReviewEvents = typeof PR_REVIEW_CREATED;
+type IssueCommentContext = Context<typeof ISSUE_COMMENT_CREATED>
+type CommitCommentCreatedContext = Context<typeof COMMIT_COMMENT_CREATED>;
+type PrDiffCommentCreatedContext = Context<typeof PR_DIFF_COMMENT_CREATED>;
+type PrReviewCreatedContext = Context<typeof PR_REVIEW_CREATED>;
+
+type CommentContext = IssueCommentContext | CommitCommentCreatedContext | PrDiffCommentCreatedContext;
+type ReviewContext = PrReviewCreatedContext;
+
+type AnyContext = CommentContext | ReviewContext;
+
+type CommentPayload = IssueCommentCreatedEvent | CommitCommentCreatedEvent | PullRequestReviewCommentCreatedEvent;
+type ReviewPayload = PullRequestReviewSubmittedEvent;
 
 /* Generate tom image URL: */
 const getTomUrl = async () => {
-  const response = await fetch('https://api.thaas.io/api/v1/integrations/github');
+  const response = await fetch(API_URL);
   const data = await response.json() as Record<string, any>;
   const imageUrl = data?.imageUrl;
 
@@ -25,21 +44,37 @@ const getTomUrl = async () => {
   return imageUrl;
 }
 
-/* Generate comment text: */
-const getCommentBody = async () => {
-  const imageUrl = await getTomUrl();
+const getOriginalCommentUrl = (context: AnyContext): string => {
+  const commentPayload = context.payload as CommentPayload;
+  if (commentPayload.comment) return commentPayload.comment.html_url;
 
-  const body = `![tom hanks](${imageUrl})`;
-
-  return body;
+  const reviewPayload = context.payload as ReviewPayload;
+  return reviewPayload.review.html_url;
 }
 
-const isValidComment = (context: Context<CommentEvents>) => hasCommand(context.payload.comment.body) && !context.isBot;
-const isValidReview = (context: Context<ReviewEvents>) => hasCommand(context.payload.review.body) && !context.isBot;
+/* Generate comment text: */
+const getCommentBody = async (context: AnyContext) => {
+  const imageUrl = await getTomUrl();
+  const originalCommentUrl = getOriginalCommentUrl(context);
+
+  // const body = `![tom hanks](${imageUrl})\n\n<sup>I am a [bot](${HOMEPAGE})</sup>, responding to a [comment](${originalCommentUrl}).`;
+
+  return '';
+}
+
+const isValid = (context: AnyContext) => {
+  if (context.isBot) return false;
+
+  const commentPayload = context.payload as CommentPayload;
+  if (commentPayload.comment) return hasCommand(commentPayload.comment.body);
+
+  const reviewPayload = context.payload as ReviewPayload;
+  return hasCommand(reviewPayload.review.body);
+};
 
 /* Create comment on an issue: */
-const createIssueComment = async (context: Context<typeof ISSUE_COMMENT_CREATED | typeof PR_REVIEW_CREATED>) => {
-  const body = await getCommentBody();
+const createIssueComment = async (context: AnyContext) => {
+  const body = await getCommentBody(context);
 
   return context.octokit.issues.createComment(
     context.issue({ body })
@@ -47,8 +82,8 @@ const createIssueComment = async (context: Context<typeof ISSUE_COMMENT_CREATED 
 }
 
 /* Reply to a PR comment (in the diff): */
-const createPrDiffComment = async (context: Context<typeof PR_DIFF_COMMENT_CREATED>) => {
-  const body = await getCommentBody();
+const createPrDiffComment = async (context: PrDiffCommentCreatedContext) => {
+  const body = await getCommentBody(context);
 
   return context.octokit.rest.pulls.createReplyForReviewComment(
     context.pullRequest({
@@ -60,7 +95,7 @@ const createPrDiffComment = async (context: Context<typeof PR_DIFF_COMMENT_CREAT
 
 /* Reply to a comment on a commit (not in PR): */
 const createCommitComment = async (context: Context<typeof COMMIT_COMMENT_CREATED>) => {
-  const body = await getCommentBody();
+  const body = await getCommentBody(context);
 
   const { commit_id: commit_sha, path, position } = context.payload.comment;
 
@@ -77,41 +112,33 @@ const createCommitComment = async (context: Context<typeof COMMIT_COMMENT_CREATE
 /* Main: */
 export = (app: Probot) => {
   app.on(ISSUE_COMMENT_CREATED, async (context) => {
-    console.log('ISSUE_COMMENT_CREATED START')
-    app.log.info(context.payload);
-    console.log('ISSUE_COMMENT_CREATED END')
+    console.log('ISSUE_COMMENT_CREATED START'); app.log.info(context.payload); console.log('ISSUE_COMMENT_CREATED END'); // TODO: REMOVE
 
-    if (!isValidComment(context)) return;
+    if (!isValid(context)) return;
 
     await createIssueComment(context);
   });
 
   app.on(PR_DIFF_COMMENT_CREATED, async (context) => {
-    console.log('PR_DIFF_COMMENT_CREATED START')
-    app.log.info(context.payload);
-    console.log('PR_DIFF_COMMENT_CREATED END')
+    console.log('PR_DIFF_COMMENT_CREATED START'); app.log.info(context.payload); console.log('PR_DIFF_COMMENT_CREATED END'); // TODO: REMOVE
 
-    if (!isValidComment(context)) return;
+    if (!isValid(context)) return;
 
     await createPrDiffComment(context);
   });
 
   app.on(COMMIT_COMMENT_CREATED, async (context) => {
-    console.log('COMMIT_COMMENT_CREATED START')
-    app.log.info(context.payload);
-    console.log('COMMIT_COMMENT_CREATED END')
+    console.log('COMMIT_COMMENT_CREATED START'); app.log.info(context.payload); console.log('COMMIT_COMMENT_CREATED END'); // TODO: REMOVE
 
-    if (!isValidComment(context)) return;
+    if (!isValid(context)) return;
 
     await createCommitComment(context);
   });
 
   app.on(PR_REVIEW_CREATED, async (context) => {
-    console.log('PR_REVIEW_CREATED START')
-    app.log.info(context.payload);
-    console.log('PR_REVIEW_CREATED END')
+    console.log('PR_REVIEW_CREATED START'); app.log.info(context.payload); console.log('PR_REVIEW_CREATED END'); // TODO: REMOVE
 
-    if (!isValidReview(context)) return;
+    if (!isValid(context)) return;
 
     // Note: we use the same API call as we do when replying to a regular issue comment, since reviews show up as an issue comment:
     await createIssueComment(context);

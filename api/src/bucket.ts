2db54481229580e5ff489ce75df0ef50a2b90968
farkmarnum <mark@nayya.com>
2022-08-14 19:13:42 -0400

    Change name

--- api/src/bucket.ts
+++ api/src/bucket.ts
@@ -2,7 +2,7 @@ import * as aws from '@pulumi/aws';
 import { S3_BUCKET_NAME } from './config';
 
 const createBucket = () =>
-  new aws.s3.Bucket('b', {
+  new aws.s3.Bucket('imagesBucket', {
     acl: 'public-read',
     bucket: S3_BUCKET_NAME,
   });

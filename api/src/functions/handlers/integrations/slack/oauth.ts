2d8376f43b8394f973cbe0818ba3dbdee97fbaf4
farkmarnum <mark@nayya.com>
2022-08-21 17:07:26 -0400

    Fix

--- api/src/functions/handlers/integrations/slack/oauth.ts
+++ api/src/functions/handlers/integrations/slack/oauth.ts
@@ -2,10 +2,9 @@ import { getInstaller } from '../../../helpers/slack';
 import wrapReqRes from '../../../helpers/wrapReqRes';
 import { Handler } from '../../../../types';
 
-const handler: Handler = async (event, context) =>
-  wrapReqRes(async (req, res) => {
-    const installer = getInstaller();
-    await installer.handleCallback(req, res);
-  })(event, context);
+const handler: Handler = wrapReqRes(async (req, res) => {
+  const installer = getInstaller();
+  await installer.handleCallback(req, res);
+});
 
 export default handler;
--- api/src/functions/helpers/wrapReqRes.ts
+++ api/src/functions/helpers/wrapReqRes.ts
@@ -1,32 +1,33 @@
-import * as express from 'express';
+import { Request, Response } from 'express';
 import { configure } from '@vendia/serverless-express';
 import { Context } from 'aws-lambda';
 import { Handler } from '../../types';
 
 const wrapReqRes = (
-  callback: (req: express.Request, res: express.Response) => Promise<void>,
+  callback: (req: Request, res: Response) => Promise<void>,
 ) => {
-  const app = express();
-
-  app.all('/*', async (req, res) => {
-    await callback(req, res);
-  });
-
   const handler: Handler = (event, context) =>
     new Promise((resolve, reject) => {
-      configure({ app })(
-        event,
-        context as unknown as Context, // serverlessExpress still supports some deprecated params and requires them to be present, so we're aggressively casting the type here
-        (err, result) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(result);
-          }
-        },
-      );
-    });
+      import('express').then((express) => {
+        const app = express();
 
+        app.all('/*', async (req, res) => {
+          await callback(req, res);
+        });
+
+        configure({ app })(
+          event,
+          context as unknown as Context, // serverlessExpress still supports some deprecated params and requires them to be present, so we're aggressively casting the type here
+          (err, data) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(data);
+            }
+          },
+        );
+      });
+    });
   return handler;
 };
 
